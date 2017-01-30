2453324e851b4266cd85efeceb61b9f9b21c300b
nemtiax <nemtiax@gmail.com>
2017-01-30 15:20:26 -0500

    added utils for reading data

--- barkTrain.py
+++ barkTrain.py
@@ -1,2 +1,6 @@
 class Trainer(object):
-    def __init__()
+    def __init__(self):
+
+    def load_data(self):
+        self.data_files = glob(os.path.join("./data", "tiles", "*.png"))
+        self.data = [get_image(sample_file, self.config.image_size, is_crop=self.config.is_crop, resize_w=self.config.output_size, is_grayscale = (self.config.c_dim==1)) for sample_file in sample_files]
